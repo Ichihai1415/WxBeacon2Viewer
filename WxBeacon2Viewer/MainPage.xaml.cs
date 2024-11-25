f2defaffb9945f4df1276052da2b8ea42a9e9382
Ichihai1415 <projects3141.gif@gmail.com>
2024-11-25 20:09:23 +0900

    プロジェクト ファイルを追加します。

---
+++ WxBeacon2Viewer/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Weathernews.Sensor;
+using Windows.UI.Core;
+
+namespace WxBeacon2Viewer
+{
+    public sealed partial class MainPage : Page
+    {
+        private WxBeacon2Watcher wxBeacon2Watcher = new WxBeacon2Watcher();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            wxBeacon2Watcher.Received += WxBeacon2Watcher_Found;
+        }
+
+        private async void WxBeacon2Watcher_Found(object sender, WxBeacon2 beacon)
+        {
+            var latest = await beacon.GetLatestDataAsync();
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                textBlock.Text = latest.ToString();
+                beacon.Dispose();
+            });
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "searching...";
+            wxBeacon2Watcher.Start();
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "exiting...";
+            wxBeacon2Watcher.Stop();
+        }
+    }
+}
